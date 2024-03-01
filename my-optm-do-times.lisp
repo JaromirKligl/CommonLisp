e9fe96781572f9c9c65a4e182cd5fe790183b18e
JaromirKligl <155422139+JaromirKligl@users.noreply.github.com>
2024-03-01 19:52:47 +0100

    Update my-optm-do-times.lisp

--- my-optm-do-times.lisp
+++ my-optm-do-times.lisp
@@ -1,3 +1,23 @@
+; 1. Brezna
+; 1000x hezci a lepsi 
+; pridany komentare
+( defmacro my-dotimes ( ( name times &optional result ) &body body )  
+  ( let (( stack '() ))  ; compile-time promena nezpusobuje capture
+    ( if ( and ( numberp times )
+               ( < times 11 ))
+               ;And zmenen poradi actually fixne problem se slozenimy vyrazy 
+               ; pokud by se prvni testovalo times < 11 tak by to spadlo pri vyrazu ( print 5 )
+        ( progn
+          ( push result stack)
+          ( dotimes ( loop-var times)
+            ( push `( incf ,name ) stack )
+            ( setf stack ( append body stack ) ))
+          ( append `( let  (( ,name 0 )) ) stack )) ; Append je lepsi reseni nez dolist push xd how obv
+      `( dotimes ( ,name ,times ,result ) ,@body ))))
+
+
+
+
 ;
 ; 29. Unora , tahle verze je hnusna ale funguje , je to 1. verze tady ale mozna i posledni , kdo vy estli to udelam hezcejc :)
 ;
